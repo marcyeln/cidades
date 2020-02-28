972f06805898a526d52b8da8a37b6f03befa15a2
marcyeln <carmoaparecida20@gmail.com>
2020-02-28 20:39:25 -0300

    mudou a descrição

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mongaguá!");
+            Console.WriteLine("itanhaém!");
+            Console.WriteLine("peruíbe!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cidades
-solução do exercicio pc1 etec adolpho berezin 
+
+exercício de informática na Etec Adolpho Berezin
+
+Faça um programa que exiba os nomes das cidades do litoral sul de São Paulo, um em cada linha.
